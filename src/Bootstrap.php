9a3eda3fd5762af45500c47c648b4fcd1c162e68
Samuel Heinzmann <samuel.heinzmann@swisscom.com>
2016-08-30 23:24:03 +0200

    Implemented Dependency Injector

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -27,8 +27,10 @@ if ($environment !== 'production') {
 }
 $whoops->register();
 
-$request = new HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
-$response = new HttpResponse();
+$injector = include 'Dependencies.php' ;
+
+$request = $injector->make('Http\HttpRequest');
+$response = $injector->make('Http\HttpResponse');
 
 $routeDefinitionCallback = function (\FastRoute\RouteCollector $r) {
     $routes = include('Routes.php');
@@ -53,7 +55,7 @@ switch ($routeInfo[0]) {
         $method = $routeInfo[1][1];
         $vars = $routeInfo[2];
 
-        $class = new $className($response);
+        $class = $injector->make($className);
         $class->$method($vars);
         break;
 }
