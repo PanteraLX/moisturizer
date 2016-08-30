e75331de52606f885ba43ce22ca4c3996e6df9f8
Samuel Heinzmann <samuel.heinzmann@swisscom.com>
2016-08-30 22:31:11 +0200

    Added Whoops logger to bootstrap file

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,3 +1,28 @@
 <?php
 
-echo 'test';
+namespace Moisturizer;
+
+use Exception;
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
+
+require '../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$environment = 'development';
+
+/**
+ * Register the error handler
+ */
+$whoops = new Run();
+if ($environment !== 'production') {
+    $whoops->pushHandler(new PrettyPageHandler());
+} else {
+    $whoops->pushHandler(function($e){
+        echo 'Friendly error page and send an email to the developer';
+    });
+}
+$whoops->register();
+
+throw new Exception;
